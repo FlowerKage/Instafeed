08957926b56917b19f28926e0f599c22152f9906
FlowerKage <43425134+FlowerKage@users.noreply.github.com>
2020-06-16 18:35:38 +0200

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","insta_feed");
+    $conn = mysqli_connect("cdm1s48crk8itlnr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","zlnyevkkvj5gm1qv","zin8qylrxlcdpu1d","csfqmlt4zokfcaej");
     if(!$conn)
     {
         echo "Connection failed";
